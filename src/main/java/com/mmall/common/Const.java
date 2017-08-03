774c85d598394f33d14b2956dfe169eeb8fb23d2
codeagles <hcn0521@outlook.com>
2017-08-03 23:21:50 +0800

    创建订单功能

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -84,6 +84,15 @@ public class Const {
 
         public void setValue(String value) {
             this.value = value;
+
+        }
+        public static OrderStatusEnum codeOf(int code){
+            for(OrderStatusEnum orderStatusEnum : values()){
+                if(orderStatusEnum.getCode() == code){
+                    return orderStatusEnum;
+                }
+            }
+            throw new RuntimeException("没有找到对应的枚举");
         }
     }
 
@@ -122,4 +131,40 @@ public class Const {
             this.value = value;
         }
     }
+
+    public enum PaymentTypeEnum{
+        ONLINE_PAY(1,"在线支付");
+
+        private int code;
+        private String value;
+
+        public int getCode() {
+            return code;
+        }
+
+        public void setCode(int code) {
+            this.code = code;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+        PaymentTypeEnum(int code,String value){
+            this.code = code;
+            this.value = value;
+        }
+        public static PaymentTypeEnum codeOf(int code){
+            for(PaymentTypeEnum paymentTypeEnum : values()){
+                if(paymentTypeEnum.getCode() == code){
+                    return paymentTypeEnum;
+                }
+            }
+            throw new RuntimeException("没有找到对应的枚举");
+        }
+    }
+
 }
--- src/main/java/com/mmall/dao/CartMapper.java
+++ src/main/java/com/mmall/dao/CartMapper.java
@@ -29,4 +29,6 @@ public interface CartMapper {
     int checkedOrUnCheckedProduct(@Param("userId") Integer userId,@Param(value = "productId") Integer productId,@Param("checked") Integer checked);
 
     int selectCartProductCount(Integer userId);
+
+    List<Cart> selectCheckedCartbyUserId(Integer userId);
 }
