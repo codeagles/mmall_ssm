2a75c7ef5d64eee2eea83fbd225f1dd26a0d90fa
codeagles <hcn0521@outlook.com>
2017-08-03 22:10:23 +0800

    支付宝对接回调

--- src/main/java/com/mmall/service/IOrderService.java
+++ src/main/java/com/mmall/service/IOrderService.java
@@ -2,10 +2,14 @@ package com.mmall.service;
 
 import com.mmall.common.ServerResponse;
 
+import java.util.Map;
+
 /**
  * Created by codeagles on 2017/8/2.
  */
 public interface IOrderService {
     ServerResponse pay(Long orderNo, Integer userId, String path);
+    ServerResponse aliCallback(Map<String,String> params);
+    ServerResponse queryOrderPayStatus(Integer userId, Long orderNo);
 
 }
